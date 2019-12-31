3939641d67facb4f736c93e4c89099e7233e00ca
Camilo P <kmilopv@gmail.com>
2019-12-31 11:32:59 -0500

    Dic-2019

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,21 +11,23 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   title = 'Learn!';
   courses$: Observable<any[]>;
-  courses: AngularFireList<any[]>;
+  // courses: AngularFireList<any[]>;
   /**
    * he $ sign doesn't affect anything, but is a standard to
    * mark a variable as an Observable
    */
   constructor(db: AngularFireDatabase) {
-    // this.courses$ = db.list('  /courses').valueChanges();
-    this.courses = db.list('courses');
-    this.courses.snapshotChanges(['child_added'])
-      .subscribe(actions => {
-        actions.forEach(action => {
-          console.log(action.type);
-          console.log(action.key);
-          console.log(action.payload.val());
-        });
-      });
+    
+    this.courses$ = db.list('courses').valueChanges();
+
+    // this.courses.snapshotChanges(['child_added'])
+    //   .subscribe(actions => {
+    //     actions.forEach(action => {
+    //       console.log(action.type);
+    //       console.log(action.key);
+    //       console.log(action.payload.val());
+    //     });
+    //     console.log(actions);
+    //   });
   }
 }
