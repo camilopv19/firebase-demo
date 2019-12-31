b0771dd50ca6506ba02c749e8f940e271c4da2f7
Camilo P <kmilopv@gmail.com>
2019-12-31 09:47:04 -0500

    Firebase 2020 demo continues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+// import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firebaseDemo';
+  title = 'Learn!';
+  courses$: Observable<any[]>;
+  courses: AngularFireList<any[]>;
+  /**
+   * he $ sign doesn't affect anything, but is a standard to
+   * mark a variable as an Observable
+   */
+  constructor(db: AngularFireDatabase) {
+    // this.courses$ = db.list('  /courses').valueChanges();
+    this.courses = db.list('courses');
+    this.courses.snapshotChanges(['child_added'])
+      .subscribe(actions => {
+        actions.forEach(action => {
+          console.log(action.type);
+          console.log(action.key);
+          console.log(action.payload.val());
+        });
+      });
+  }
 }
